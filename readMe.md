80a728937a77f5fd3fa15462c8acd6b05363d497
anupreet9 <anupreet.aashy@gmail.com>
2022-10-31 22:35:29 +0530

    Initial commit

---
+++ readMe.md
@@ -0,0 +1,11 @@
+# YouTube to Spotify Playlist Converter
+
+Java application that converts a chosen YouTube playlist from your account to a Spotify playlist
+
+## How to Use
+
+Get a Spotify OAuth Access Token and input it into "SpotifyManager.java" accessToken field
+
+Get your spotify account ID and input in userId field
+
+Run and follow instructions
---
+++ src/main/java/SpotifyManager.java
@@ -0,0 +1,29 @@
+import com.wrapper.spotify.SpotifyApi;
+import java.util.*;
+
+//********************************************************************************//
+
+public class SpotifyManager {
+    private static final String accessToken = "";
+    private static final String userId = "";
+
+    public SpotifyApi getSpotifyApi(){
+        //SpotifyApi class gives access to spotify web api
+        //.builder is a class that builds spotifyApi instances
+        final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+                .setAccessToken(accessToken)
+                .build();
+
+        return spotifyApi;
+    }
+
+    public String getAccessToken(){
+        return accessToken;
+    }
+
+    public String getUserId(){
+        return userId;
+    }
+
+
+}
